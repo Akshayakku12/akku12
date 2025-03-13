71e04009fb7d0c4236201dcfb4d891cbfcd4e237
Akshayakku12 <Akshayravi660@gmail.com>
2025-03-13 21:56:20 +0530

    hi

---
+++ Welcome.java
@@ -0,0 +1,7 @@
+public class Welcome {
+    public static void main(String[] args) {
+        int Myint = 9; // Declare an integer variable
+        double Mydouble = Myint; // Implicitly convert the int to a double
+        System.out.println(Mydouble); // Print the value of Mydouble
+    }
+}
