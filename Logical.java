4f7e6b9357af189b877d44fe9edd3499fcad090d
Akshayakku12 <Akshayravi660@gmail.com>
2025-03-13 22:21:45 +0530

    hiiiii

---
+++ Logical.java
@@ -0,0 +1,8 @@
+public class Logical {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 20;
+        boolean result = (A > B) && (A != B); // Fixed the logical expression
+        System.out.println("(A > B) && (A != B): " + result); // Adjusted the output message
+    }
+}
