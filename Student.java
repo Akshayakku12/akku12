4a7a9333ed2a5353b83d20e523b1080bbc0f89fb
Akshayakku12 <Akshayravi660@gmail.com>
2025-03-13 21:49:01 +0530

    hello

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+public static void main(String[]args) {
+    String name="Akshay";
+    String Subjectname="java";
+    int Mark=90;
+    String Grade="O";
+    System.out.println("name");
+    System.out.println("Subjectname");
+    System.out.println(Mark);
+    System.out.println(Grade);
+}
+}
