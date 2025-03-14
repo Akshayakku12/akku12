35b45755c74a33656076dbc8a9761b2f15d0521c
Akshayakku12 <akshayravi660@gmail.com>
2025-03-14 14:46:49 +0530

    hii

---
+++ Rename.java
@@ -0,0 +1,9 @@
+public class Rename {
+    public static void main(String[]args) {
+        String[]fruits={"apple","mango"};
+        fruits[1]="coconut";
+        System.out.println(fruits[1]);
+        
+    }
+}
+
