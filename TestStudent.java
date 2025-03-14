ac7f0bb3dcd7ac76b311e29f5ad71eaaf01b57c6
Akshayakku12 <akshayravi660@gmail.com>
2025-03-14 15:44:53 +0530

    hii

---
+++ TestStudent.java
@@ -0,0 +1,15 @@
+    class Student {
+        int id;
+        String name;
+    }
+
+   public class TestStudent
+     {
+        public static void main(String[] args) 
+        {
+            Student S1 = new Student(); 
+            S1.id = 101; 
+            System.out.println(S1.id); 
+        }
+    }
+
