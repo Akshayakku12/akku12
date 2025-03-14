896a496987b6321029905831f667c83686c97f22
Akshayakku12 <akshayravi660@gmail.com>
2025-03-14 13:58:11 +0530

    hi

---
+++ Array1.java
@@ -0,0 +1,7 @@
+public class Array1 {
+    public static void main(String[] args) {
+        int[] a = {2, 3, 4};
+        String[] fruits = {"apple", "mango"};
+        System.out.println(a.length);
+    }
+}
