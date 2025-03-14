dea42a6d5e8483740b3c643174f227de5d8be8c5
Akshayakku12 <akshayravi660@gmail.com>
2025-03-14 10:09:41 +0530

    hello

---
+++ Vowels.java
@@ -0,0 +1,10 @@
+public class Vowels {
+    public static void main(String[] args) {
+        char ch = 'r'; // Corrected the character declaration
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') { // Fixed the 'if' condition
+            System.out.println("It is a Vowel");
+        } else {
+            System.out.println("It is not a Vowel");
+        }
+    }
+}
