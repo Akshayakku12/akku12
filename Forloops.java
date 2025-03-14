dc98bea83ffcf88e338bc201d8824437b377a6f1
Akshayakku12 <akshayravi660@gmail.com>
2025-03-14 11:29:54 +0530

    how are you

---
+++ Forloops.java
@@ -0,0 +1,12 @@
+public class Forloops {
+    public static void main(String[] args) {  
+        int i = 0; 
+        while (i <= 5) { 
+            System.out.println(i); 
+            i++; 
+        }  
+        for (i = 0; i <= 5; i++) { 
+            System.out.println(i); 
+        }
+    }
+}
