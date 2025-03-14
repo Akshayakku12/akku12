1baff3786cfe2d2e56a46d755c4320a1e3abaee7
Akshayakku12 <akshayravi660@gmail.com>
2025-03-14 10:49:11 +0530

    hello

---
+++ Dowhile.java
@@ -0,0 +1,9 @@
+public class Dowhile {
+    public static void main(String[] args) {
+        int number = 1;
+        do {
+            System.out.println("Number: " + number);
+            number++;
+        } while (number <= 5);
+    }
+}
