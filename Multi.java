c303f253e6b8b704d53f6b475c51b16d9e2d4ed2
Akshayakku12 <akshayravi660@gmail.com>
2025-03-14 14:17:32 +0530

    hiii

---
+++ Multi.java
@@ -0,0 +1,6 @@
+public class Multi {
+    public static void main(String[] args) {
+       String[][] game = {{"cricket", "luddo"}, {"carrom", "football"}};
+       System.out.println(game[1][1]);
+    }
+}
