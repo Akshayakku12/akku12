d33e2db14925c301e8c19b27278fc5ecc315d392
Akshayakku12 <akshayravi660@gmail.com>
2025-03-14 11:48:29 +0530

    hello

---
+++ Break.java
@@ -0,0 +1,14 @@
+public class Break {
+    public static void main(String[] args) {  
+        int i = 0; 
+        while (i <= 5) 
+            for (i = 0; i <= 5; i++) {
+                if(i==6)
+                break;
+            System.out.println(i); 
+            i++; 
+         
+            }
+    }
+}
+            
