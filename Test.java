e3a2560b8ac23fc41b72975e599abb59517b85b3
akshayakku@12 <akshayravi660@gmail.com>
2025-03-15 10:11:21 +0530

    hello

---
+++ Test.java
@@ -0,0 +1,13 @@
+class Animal {
+}
+
+class Dog extends Animal {
+    }
+
+
+public class Test {
+    public static void main(String[] args) {
+        Dog myDog = new Dog();
+        System.out.println(myDog);
+    }
+}
