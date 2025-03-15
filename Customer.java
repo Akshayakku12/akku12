9946f8ca0cf5d32927f1961243c5cb0f12b97d4e
Akshayakku12 <akshayravi660@gmail.com>
2025-03-15 10:45:42 +0530

    hello

---
+++ Customer.java
@@ -0,0 +1,10 @@
+public class Customer {
+    public static void main(String[]args) {
+        int i;
+        int[] Customerid={100,101,102,103,104};
+        for(i=0;i<=5;i++)
+        {
+            System.out.println(Customerid[i]);
+        }
+    }
+}
